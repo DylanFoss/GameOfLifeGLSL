b1b091c7bbedf36829e85cf697ad3247ff8e4331
DylanFoss <57671477+DylanFoss@users.noreply.github.com>
2022-12-13 11:08:50 +0000

    Fixed Shader classes error message to use std::string's by resizing the string.

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,7 @@
 #include "GameOfLife.h"
 int main()
 {
-	std::unique_ptr<GameOfLife> app = std::make_unique<GameOfLife>("Game Of Life", 800, 800, 40, 40);
+	std::unique_ptr<GameOfLife> app = std::make_unique<GameOfLife>("Game Of Life", 800, 800, 200, 200);
 	app->Run();
 
 	return 0;
