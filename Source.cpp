45a0e4d776325343944145bbd275c203074a2a42
DylanFoss <57671477+DylanFoss@users.noreply.github.com>
2023-01-17 06:53:21 +0000

    added basic command line parameter support

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,20 @@
 #include "GameOfLife.h"
-int main()
+int main(int argc, char* argv[])
 {
-	std::unique_ptr<GameOfLife> app = std::make_unique<GameOfLife>("Game Of Life", 800, 800, 200, 200);
+	int initArray[4] = { 800, 800, 400, 400 };
+
+	if (argc > 1)
+	{
+		if (argc < 5)
+		{
+			for (int i = 1; i < argc; i++)
+			{
+				initArray[i - 1] = std::stoi(argv[i]);
+			}
+		}
+	}
+
+	std::unique_ptr<GameOfLife> app = std::make_unique<GameOfLife>("Game Of Life", initArray[0], initArray[1], initArray[2], initArray[3]);
 	app->Run();
 
 	return 0;
